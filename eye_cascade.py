eb1ae5f43ba66b7bbe5a007edca76f508c943384
Ismar <76652264+ismarapw@users.noreply.github.com>
2022-08-03 12:36:38 +0700

    init-project

---
+++ eye_cascade.py
@@ -0,0 +1,33 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y , w ,h) in faces:
+
+        # Set boundary box for faces
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+        # Detect Eyes
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey ,ew, eh) in eyes:
+            # Set boundary box for eyes
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+
+            print("eye detected")
+
+    # Display the output
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
