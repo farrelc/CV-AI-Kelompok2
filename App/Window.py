c172d26265d891eb6546864607e353cf341c6191
Ismar <76652264+ismarapw@users.noreply.github.com>
2022-12-07 16:16:12 +0700

    changing-model

--- App/Window.py
+++ App/Window.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk,ImageDraw, ImageFont
 import cv2
 from Model import Detector
 
@@ -12,8 +12,9 @@ class Window(tk.Tk):
     start = False
     counter = 0
     face_is_detected = False
-    eye_is_detected = False
+    eye_is_closed = False
     time_threshold = 0
+    time_to_reset = 0
 
     def __init__(self):
         super().__init__()
@@ -29,7 +30,7 @@ class Window(tk.Tk):
 
         # Webcam
         self.canvas = tk.Canvas(bg = "black", height=480, width=640)
-        self.canvas.create_text(330, 240, text="Camera is Turned Off", fill="white")
+        self.canvas.create_text(330, 240, text="Camera is Turned Off", fill="white",font=("Helvetica", 15))
         self.canvas.grid(row = 0, column = 0, padx=10, pady=10, rowspan = 9)
 
         # button
@@ -56,6 +57,7 @@ class Window(tk.Tk):
         # Reset Button
         self.reset_btn = ttk.Button(text = "Reset", command = self.reset_btn_action)
         self.reset_btn.grid(row = 5, column=1)
+        self.reset_btn['state'] = 'disabled'
 
         # Timer info
         self.timer_label = ttk.Label(text= "Time of Eye interacted with The Screen :")
@@ -65,29 +67,32 @@ class Window(tk.Tk):
         self.timer_seconds = tk.Label(text = "00 Secs", font=("Helvetica", 20))
         self.timer_seconds.grid(row=8, column=1)
 
+
     
     def toggle_webcam(self):
         if(self.cam_on):
             self.cam_on = False
             self.vid.release()
             self.canvas = tk.Canvas(bg = "black", height=480, width=640)
-            self.canvas.create_text(330, 240, text="Camera is Turned Off", fill="white")
+            self.canvas.create_text(330, 240, text="Camera is Turned Off", fill="white", font=("Helvetica", 15))
         else :
             self.cam_on = True
             self.vid = cv2.VideoCapture(0) 
             self.canvas = ttk.Label()
             self.show_frame()
             self.start = False
+            
 
         self.canvas.grid(row = 0, column = 0, padx=2, pady=2, rowspan=9)
     
 
     def start_btn_action(self):
-        if (self.cam_on) :
+        if (self.cam_on and not self.start) :
             self.start = True
             self.set_selected_time_threshold()
-            print(self.time_threshold)
             self.counter_label()
+            self.reset_btn['state'] = 'disabled'
+            
     
     
     def set_selected_time_threshold(self):
@@ -102,59 +107,101 @@ class Window(tk.Tk):
     def counter_label(self):
         def count():
             if(self.start and self.cam_on):
+
+                # Check face and eyes
+                print(self.eye_is_closed)
+                if(self.face_is_detected == False or self.eye_is_closed == True):
+                    # if face is not deteced or eye is closed then do not update time counter but update time reset
+                    self.time_to_reset += 1
+                else :
+                    # Update counter and time to reset if face or eye is detected again
+                    self.counter += 1
+                    self.time_to_reset = 0
+
+                # Update Second and Minutes from counter
                 secs = self.counter % 60
                 mins = self.counter // 60
+
+                # Update Timer GUI
                 new_label_seconds = str(secs) if secs > 9 else "0" + str(secs)
                 new_label_minutes = str(mins) if mins > 9 else "0" + str(mins)
                 self.timer_minutes['text'] = new_label_minutes + " Mins"
                 self.timer_seconds['text'] = new_label_seconds + " Secs"
-
-                if(self.counter == self.time_threshold):
-                    messagebox.showinfo("Time is Up","Lets see around before you get back into the screen")
+                
+                # Reset time if face and eye not deteced for certain time              
+                if(self.time_to_reset == 5):
+                    self.start = False
+                    self.time_to_reset = 0
+                    self.reset_btn_action()
+                    messagebox.showinfo("Reset", "We Reset the Timer because you were not in front of the Screen")
                     return 
 
-                print("Face is Detected : ",self.face_is_detected)
-                print("Eye is Detected : ",self.eye_is_detected)
-
-                if(not(self.face_is_detected or self.eye_is_detected)):
-                    self.counter -= 1
-
-                self.counter += 1
-
+                # Check Time limit
+                if(self.counter == self.time_threshold):
+                    self.start = False
+                    self.reset_btn_action()
+                    messagebox.showinfo("Time is Up", f"Its been {self.time_choosen.get()}, Lets see around before you get back into the screen")
+                    return 
+    
+                # Call function recursively
                 self.timer_seconds.after(1000, count)
 
         count()
 
 
     def stop_btn_action(self):
-        self.start = False
+        if(self.start):
+            self.start = False
+            self.reset_btn['state'] = 'normal'
     
     def reset_btn_action(self):
         self.counter = 0
         self.timer_minutes['text'] = "00 Mins"
         self.timer_seconds['text'] = "00 Secs"
-
+    
+    
     def show_frame(self):
         if(self.cam_on):
 
-            ret, _ = self.vid.read()
+            ret, frame = self.vid.read()
             if(ret):
 
                 # Get the latest frame and convert into Image
-                cv2image = cv2.cvtColor(self.vid.read()[1],cv2.COLOR_BGR2RGB)
-                cv2image = cv2.resize(cv2image, (640,480))
+                cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                cv2image = cv2.resize(cv2image, (640,480), interpolation=cv2.INTER_CUBIC)
 
                 if (self.start):
-                    rendered_image, self.face_is_detected, self.eye_is_detected = Detector.get_face_and_eye(cv2image)
-                else:
-                    rendered_image = cv2image
+                    rendered_image, self.face_is_detected, self.eye_is_closed = Detector.get_face_and_eye(cv2image)
+                    img = Image.fromarray(rendered_image)
+                    final_img = self.face_eye_stats(img)
 
-                img = Image.fromarray(rendered_image)
+                else:
+                    final_img = Image.fromarray(cv2image)
+
 
                 # Convert image to PhotoImage
-                imgtk = ImageTk.PhotoImage(image = img)
+                imgtk = ImageTk.PhotoImage(image = final_img)
                 self.canvas.imgtk = imgtk
                 self.canvas.configure(image=imgtk)
-
+                
                 # Repeat after an interval to capture continiously
-                self.canvas.after(20, self.show_frame)
+                self.canvas.after(10, self.show_frame)
+    
+    
+    def face_eye_stats(self, frame):
+        draw = ImageDraw.Draw(frame)
+        if(self.face_is_detected):
+            if(self.eye_is_closed == False):
+                text = "Face Detected and Eyes Opened"
+            else:
+                text = "Face Detected but Eyes Closed"
+            padding_bottom = 5
+        else:
+            text = "Face Not Detected"
+            padding_bottom = 10
+
+        left, top, right, bottom = draw.textbbox((10,10), text = "Status: " + text, font= ImageFont.truetype('arial.ttf', 25))
+        draw.rectangle((left-5, top-5, right+5, bottom+padding_bottom), fill="black")
+        draw.text((10,10), text = "Status: " + text, font=ImageFont.truetype('arial.ttf', 25), fill="white")
+
+        return frame
--- Model/Detector.py
+++ Model/Detector.py
@@ -1,34 +1,101 @@
-import cv2
+import cv2 as cv
+import mediapipe as mp
+import math
+import numpy as np
+  
+
+# landmark detection function 
+def landmarksDetection(img, results):
+    img_height, img_width= img.shape[:2]
+    # list[(x,y), (x,y)....]
+    mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results.multi_face_landmarks[0].landmark]
+
+    # returning the list of tuples for each landmarks 
+    return mesh_coord
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+
+# Blinking Ratio
+def closingEyeRatio(landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom) + 1e-8
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom) + 1e-8
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio) / 2
+    return ratio 
+
 
 def get_face_and_eye(cv2image):
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
-    gray = cv2.cvtColor(cv2image, cv2.COLOR_BGR2GRAY)
 
-    face_detected = False
-    eye_detected = False
+    # face mesh model
+    map_face_mesh = mp.solutions.face_mesh
 
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-    for (x, y , w ,h) in faces:
+    # constants
+    EYE_CLOSED = False
+    FACE_DETECTED = False
 
-        # Set boundary box for faces
-        cv2.rectangle(cv2image, (x,y), (x+w, y+h), (255, 0 , 0), 3)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = cv2image[y:y+h, x:x+w]
+    # Left eyes indices 
+    LEFT_EYE =[362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398]
 
-        # face detected
-        face_detected = True
+    # right eyes indices
+    RIGHT_EYE=[33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246]
 
-        # Detect Eyes
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 11)
-        for (ex, ey ,ew, eh) in eyes:
-            # Set boundary box for eyes
-            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+    # Face
+    FACE_OVAL= [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
 
-            # eye detected
-            eye_detected = True
-    
-    return cv2image, face_detected, eye_detected
+    # Run face mesh Model
+    with map_face_mesh.FaceMesh(min_detection_confidence = 0.7, min_tracking_confidence=0.1) as face_mesh:
 
-    
+        #  resizing frame
+        frame = cv.resize(cv2image, None, fx=1, fy=1, interpolation=cv.INTER_CUBIC)
+        rgb_frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+
+        # Face Mesh Detection
+        results  = face_mesh.process(rgb_frame)
+        if results.multi_face_landmarks:
+            
+            # Update face detection stats
+            FACE_DETECTED = True
+
+            # Get face mesh Coordinates
+            mesh_coords = landmarksDetection(frame, results)
+
+            # Draw face and eyes in frame
+            cv.polylines(frame, [np.array([mesh_coords[p] for p in LEFT_EYE], dtype=np.int32)], True, (0,255,0), 1, cv.LINE_AA)
+            cv.polylines(frame, [np.array([mesh_coords[p] for p in RIGHT_EYE], dtype=np.int32)], True, (0,255,0), 1, cv.LINE_AA)
+            cv.polylines(frame, [np.array([mesh_coords[p] for p in FACE_OVAL], dtype=np.int32)], True, (0,255,0), 1, cv.LINE_AA)
+                        
+            # Closing Eye Ratio
+            ratio = closingEyeRatio(mesh_coords, RIGHT_EYE, LEFT_EYE)
+
+            # Eye closed stats update
+            EYE_CLOSED = True if ratio > 3.75 else False
+  
+    return frame, FACE_DETECTED, EYE_CLOSED
